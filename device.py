Current calculation and fixed transmission

device.py
@@ -85,6 +85,34 @@ def get_dx_model(mode, quantity_name, grid_name):
 
     return MultiModel(dx_qs_trafo, name)
 
+def get_E_fermi(q: QuantityDict, *, i):
+    #dos = 8*np.pi / physics.H**3 * torch.sqrt(2 * q[f'm_eff{i}']**3 * q['DeltaE'])
+
+    #best_E_f = None
+    #best_abs_remaining_charge = float('inf')
+    #E_fs = np.arange(0*physics.EV, 0.8*physics.EV, 1e-3*physics.EV)
+    #for E_f in E_fs:
+    #    fermi_dirac = 1 / (1 + torch.exp(physics.BETA * (q['DeltaE'] - E_f)))
+    #    integrand = dos * fermi_dirac
+    #    # Particle, not charge density
+    #    n = (grid_quantities.sum_dimension('DeltaE', integrand, q.grid)
+    #         * physics.E_STEP)
+    #    abs_remaining_charge = torch.abs(n - q[f'doping{i}']).item()
+    #    if abs_remaining_charge < best_abs_remaining_charge:
+    #        best_abs_remaining_charge = abs_remaining_charge
+    #        best_E_f = E_f
+
+    #    E_f += 1e-3 * physics.EV
+
+    #assert best_E_f != E_fs[0] and best_E_f != E_fs[-1], E_f / physics.EV
+
+    #relative_remaining_charge = best_abs_remaining_charge / q[f'doping{i}']
+    #print(f'Best fermi energy: {best_E_f / physics.EV} eV with a relative remaining charge of {relative_remaining_charge}')
+
+    best_E_f = 0.258 * physics.EV
+
+    return best_E_f + q[f'V{i}']  #* torch.ones((1,) * q.grid.n_dim)
+
 def factors_trafo(qs, i, contact):
     """
     Use the boundary conditions to find the factors to multiply a/b_output with
@@ -178,11 +206,42 @@ def TR_trafo(qs, *, contact):
     incoming_coeff_in = q[f'{contact.incoming_coeff_in_name}{contact.index}_{contact}']
     incoming_coeff_out = q[f'{contact.incoming_coeff_out_name}{contact.index}_{contact}']
     outgoing_coeff_out = q_out[f'{contact.outgoing_coeff_out_name}{contact.out_index}_propagated_{contact}']
-    abs_v_in = q[f'abs_v{contact.index}_{contact}']
-    abs_v_out = q_out[f'abs_v{contact.out_index}_{contact}']
+    real_v_in = torch.real(q[f'v{contact.index}_{contact}'])
+    real_v_out = torch.real(q_out[f'v{contact.out_index}_{contact}'])
     q[f'T_{contact}'] = (complex_abs2(outgoing_coeff_out) / complex_abs2(incoming_coeff_in)
-                         * abs_v_out / abs_v_in)
+                         * real_v_out / real_v_in)
     q[f'R_{contact}'] = complex_abs2(incoming_coeff_out) / complex_abs2(incoming_coeff_in)
+
+    return qs
+
+def I_contact_trafo(qs, *, contact):
+    q = qs[contact.grid_name]
+    integrand = q[f'T_{contact}'] * q[f'fermi_integral_{contact}']
+    integral = (grid_quantities.sum_dimension('DeltaE', integrand, q.grid)
+                * physics.E_STEP)
+    sign = 1 if contact.name=='L' else -1
+    prefactor = -physics.Q_E / physics.H_BAR / (2*np.pi) * sign
+    q[f'I_spectrum_{contact}'] = prefactor * integrand
+    q[f'I_{contact}'] =  prefactor * integral
+
+    return qs
+
+def I_trafo(qs, *, contacts):
+    I = sum(qs[contact.grid_name][f'I_{contact}'] for contact in contacts)
+    for contact in contacts:
+        qs[contact.grid_name]['I'] = I
+
+    return qs
+
+def j_exact_trafo(qs, *, contact):
+    """
+    Calculate the exact current at the output contact
+    """
+    q = qs[f'boundary{contact.out_boundary_index}']
+    k = q[f'k{contact.out_index}_{contact}']
+    m_eff = q[f'm_eff{contact.out_index}']
+    sign = -1 if contact.name == 'L' else 1
+    q[f'j_exact_{contact}'] = sign * physics.H_BAR * k / m_eff
 
     return qs
 
@@ -194,6 +253,7 @@ class Device:
             boundaries,
             potentials,
             m_effs,
+            dopings,
         ):
         """
         boundaries: [x_b0, ..., x_bN] with N the number of layers
@@ -206,6 +266,7 @@ class Device:
         N = len(potentials)-2
         device_thickness = boundaries[-1] - boundaries[0]
         assert len(m_effs) == N+2
+        assert len(dopings) == N+2
         assert len(boundaries) == N+1
         assert sorted(boundaries) == boundaries, boundaries
 
@@ -213,6 +274,7 @@ class Device:
         self.boundaries = boundaries
         self.potentials = potentials
         self.m_effs = m_effs
+        self.dopings = dopings
 
         self.loss_functions = {}
         self.trainers = {}
@@ -335,6 +397,11 @@ class Device:
                 model_dtype = params.si_real_dtype,
                 output_dtype = params.si_real_dtype,
             )
+            models_dict[f'doping{i}'] = get_model(
+                dopings[i],
+                model_dtype = params.si_real_dtype,
+                output_dtype = params.si_real_dtype,
+            )
 
             for contact in self.contacts:
                 x_in = self.boundaries[contact.get_in_boundary_index(i)]
@@ -377,10 +444,10 @@ class Device:
                 for c in ('a', 'b'):
                     const_models_dict[i][f'{c}_output{i}_{contact}'] = one_model
                     const_models_dict[i][f'{c}_output{i}_{contact}_dx'] = zero_model
-                const_models_dict[i][f'abs_v{i}_{contact}'] = FunctionModel(
-                    lambda q, i=i, contact=contact: torch.sqrt(torch.abs(
-                        2 * (q[f'E_{contact}']-q[f'V{i}']) / q[f'm_eff{i}']
-                    ))
+                const_models_dict[i][f'v{i}_{contact}'] = FunctionModel(
+                    lambda q, i=i, contact=contact: torch.sqrt(
+                        (2 * (q[f'E_{contact}']-q[f'V{i}']) / q[f'm_eff{i}']).to(params.si_complex_dtype)
+                    )
                 )
 
         ## Output contact: Initial conditions
@@ -407,8 +474,7 @@ class Device:
                                / q[f'm_eff{i}']),
             )
             const_models_dict[i][f'E_fermi_{contact}'] = FunctionModel(
-                lambda q, i=i, contact=contact:
-                    q[f'E_{contact}'] + physics.E_FERMI_OFFSET,
+                lambda q, i=i: get_E_fermi(q, i=i),
             )
             const_models_dict[i][f'fermi_integral_{contact}'] = FunctionModel(
                 lambda q, i=i, contact=contact: 
@@ -441,6 +507,15 @@ class Device:
                     models_dict = const_models_dict[j]
                     for model_name, model in models_dict.items():
                         const_models.append(get_multi_model(model, model_name, grid_name))
+
+        # Constant MultiModels
+        for contact in self.contacts:
+            # OPTIM: unused
+            const_models.append(MultiModel(
+                j_exact_trafo,
+                f'j_exact_{contact}',
+                kwargs = {'contact': contact},
+            ))
 
 
         # Parameter-dependent models shared by multiple trainers
@@ -518,6 +593,17 @@ class Device:
                 kwargs = {'contact': contact},
             ))
 
+            shared_models.append(MultiModel(
+                I_contact_trafo,
+                f'I_{contact}',
+                kwargs = {'contact': contact},
+            ))
+
+        shared_models.append(MultiModel(
+            I_trafo,
+            f'I',
+            kwargs = {'contacts': self.contacts},
+        ))
 
         ## Bulk
         for i in range(1,N+1):
@@ -529,7 +615,7 @@ class Device:
                     FunctionModel(
                         lambda q, i=i, contact=contact:
                             (q[f'a{i}_{contact}'] * q[f'a_phase{i}_{contact}'] 
-                                + q[f'b{i}_{contact}'] * q[f'b_phase{i}_{contact}'])
+                             + q[f'b{i}_{contact}'] * q[f'b_phase{i}_{contact}'])
                     ),
                     f'phi{i}_{contact}',
                     bulk_name,
@@ -590,6 +676,7 @@ class Device:
                 f'n{i}',
                 kwargs = { 'i': i, 'grid_name': bulk_name},
             ))
+
 
         # Trainers
 

loss.py
@@ -1,3 +1,5 @@
+import torch
+
 from kolpinn.mathematics import complex_abs2, grad
 from kolpinn.grid_quantities import get_fd_second_derivative, mean_dimension, restrict
 
@@ -44,12 +46,14 @@ def SE_loss_trafo(qs, *, qs_full, with_grad, i, N, contact):
 def j_loss_trafo(qs, *, i, N, contact):
     q = qs[f'bulk{i}']
 
-    coeff_in = qs[contact.grid_name][f'{contact.incoming_coeff_in_name}{contact.index}_{contact}']
-
     prob_current = q[f'j{i}_{contact}']
     residual = prob_current - mean_dimension('x', prob_current, q.grid)
-    residual /= complex_abs2(coeff_in)
+    #coeff_in = qs[contact.grid_name][f'{contact.incoming_coeff_in_name}{contact.index}_{contact}']
+    #residual /= complex_abs2(coeff_in)
+    residual /= mean_dimension('x', complex_abs2(q[f'phi{i}_{contact}']), q.grid)
     residual /= physics.PROBABILITY_CURRENT_OOM
+    #exact_prob_current = qs[contact.out_boundary_name][f'j_exact_{contact}']
+    #residual = torch.log(complex_abs2(prob_current / exact_prob_current))
     q[f'j_loss{i}_{contact}'] = params.loss_function(residual)
 
     return qs

parameters.py
@@ -11,7 +11,7 @@ si_real_dtype = torch.float64
 si_complex_dtype = torch.complex128
 
 # Models
-loaded_parameters_index = 14
+loaded_parameters_index = None
 # Whether to use the state of the saved optimizer (possibly overwriting optimizer_kwargs)
 load_optimizer = False
 load_scheduler = False
@@ -22,19 +22,19 @@ activation_function = torch.nn.Tanh()
 model_dtype = torch.float32
 
 # Training
-max_n_training_steps = 0
+max_n_training_steps = None
 max_time = None
-min_loss = 200e-6
+min_loss = 100e-6
 report_each = 1
 Optimizer = torch.optim.LBFGS
-optimizer_kwargs = {'lr': 0.5, 'tolerance_grad': 0, 'tolerance_change': 0}
-optimizer_reset_tol = 5
+optimizer_kwargs = {'lr': 1, 'tolerance_grad': 0, 'tolerance_change': 0}
+optimizer_reset_tol = 100
 Scheduler = None # torch.optim.lr_scheduler.ReduceLROnPlateau
 scheduler_kwargs = None # {'factor': 0.5, 'patience': 5, 'cooldown': 5, 'min_lr': 1e-6, 'eps': 0}
-#loss_function = lambda x: mathematics.complex_abs2(x)**2
-#loss_aggregate_function = lambda losses: torch.sqrt(sum(losses))
 loss_function = lambda x: mathematics.complex_abs2(x)
 loss_aggregate_function = lambda losses: sum(losses)
+#loss_function = lambda x: mathematics.complex_abs2(x)**2
+#loss_aggregate_function = lambda losses: torch.sqrt(sum(losses))
 fd_first_derivatives = True
 fd_second_derivatives = True
 # Whether the voltage/energy is an input to the NN
@@ -51,4 +51,5 @@ batch_size_x = N_x
 
 # Plotting
 plot_each_voltage = 10
+plot_each_energy = 50
 extra_plots = False

physics.py
@@ -8,6 +8,7 @@ import parameters as params
 # Constants
 
 H_BAR = 1.054571817e-34 # J s
+H = H_BAR * 2*np.pi
 Q_E = 1.60217663e-19
 EV = Q_E
 M_E = 9.1093837e-31
@@ -15,21 +16,21 @@ NM = 1e-9
 K_B = 1.38064852e-23
 KELVIN = 1
 VOLT = 1
+CM = 1e-2
 
-E_MIN = 0.05 * EV
-E_STEP = 0.01 * EV
+E_MIN = 5e-4 * EV
+E_STEP = 5e-3 * EV
 E_MAX = 0.4 * EV
 E_MIN += 1e-6 * EV  # Avoiding problems at E == V (sqrt(E-V)' not defined)
 E_MAX += E_STEP / 2  # Making sure that E_MAX is used
 
-VOLTAGE_MIN = 0 * VOLT
-VOLTAGE_STEP = 0.01 * VOLT
-VOLTAGE_MAX = 0.3 * VOLT
+VOLTAGE_MIN = 0.15 * VOLT
+VOLTAGE_STEP = 0.025 * VOLT
+VOLTAGE_MAX = 0.25 * VOLT
 VOLTAGE_MAX += VOLTAGE_STEP / 2  # Making sure that VOLTAGE_MAX is used
 
 TEMPERATURE = 300 * KELVIN
 BETA = 1 / (K_B * TEMPERATURE)
-E_FERMI_OFFSET = 0.1 * EV # to be replaced by a doping-dep quantity consistent with poisson 
 
 energy_smoothing_range = 0.05 * EV
 transition_distance = 0.5 * NM
@@ -43,16 +44,19 @@ device_kwargs_dict = {
         'boundaries': [0, 5 * NM],
         'potentials': [0, 0, 0],
         'm_effs': [M_E, M_E, M_E],
+        'dopings': [0, 0, 0],
     },
     'free2': {
         'boundaries': [0, 5 * NM, 10 * NM],
         'potentials': [0, 0, 0, 0],
         'm_effs': [M_E, M_E, M_E, M_E],
+        'dopings': [0, 0, 0, 0],
     },
     'barrier1': {
         'boundaries': [0, 5 * NM],
         'potentials': [0, 0.3 * EV, 0],
         'm_effs': [0.065 * M_E, 0.1 * M_E, 0.065 * M_E],
+        'dopings': [0, 0, 0],
     },
     'rtd1': {
         'boundaries': [0 * NM, 10 * NM, 12.6 * NM, 16.6 * NM, 19.2 * NM, 29.2 * NM],
@@ -66,6 +70,7 @@ device_kwargs_dict = {
             0.065 * M_E,
             0.065 * M_E,
         ],
+        'dopings': [1e19 / CM**3, 0, 0, 1e18 / CM**3, 0, 0, 1e19 / CM**3],
     },
     'rtd1_no_tails': {
         'boundaries': [0 * NM, 2.6 * NM, 6.6 * NM, 9.2 * NM],
@@ -77,6 +82,7 @@ device_kwargs_dict = {
             0.1 * M_E,
             0.065 * M_E,
         ],
+        'dopings': [0, 0, 1e18 / CM**3, 0, 0],
     },
 }
 

visualization.py
@@ -60,12 +60,76 @@ def visualize(device):
             lines_unit = physics.VOLT, lines_unit_name = 'V',
         )
 
+    q = qs['boundary0']
+    fig, ax = plt.subplots()
+    add_lineplot(
+        ax,
+        q['I'],
+        q.grid,
+        'I',
+        'voltage',
+        quantity_unit = 1 / physics.CM**2,
+        quantity_unit_name = 'A/cm^2',
+        x_unit = physics.VOLT, x_unit_name = 'V',
+        marker = 'x',
+        linewidth = 0,
+        c='blue',
+    )
+    try:
+        matlab_path = 'matlab_results/'
+        voltages_matlab = np.loadtxt(
+                f'{matlab_path}Vbias_{params.simulated_device_name}.txt',
+            delimiter=',',
+        )
+        currents_matlab = np.loadtxt(
+            f'{matlab_path}Id_{params.simulated_device_name}.txt',
+            delimiter=',',
+        )
+        ax.plot(
+            voltages_matlab,
+            currents_matlab,
+            label='MATLAB',
+            linestyle='dashed',
+            c='blue',
+        )
+    except:
+        pass
+    ax.set_xlabel('U [V]')
+    ax.set_ylabel('I [A/cm^2]')
+    ax.grid(visible=True)
+    fig.savefig(path_prefix + f'I.pdf')
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    for contact in device.contacts:
+        q = qs[contact.grid_name]
+        add_lineplot(
+            ax,
+            q[f'I_{contact}'],
+            q.grid,
+            f'I_{contact}',
+            'voltage',
+            quantity_unit = 1 / physics.CM**2,
+            x_unit = physics.VOLT, x_unit_name = 'V',
+        )
+        ax.set_xlabel('U [V]')
+        ax.set_ylabel('I [A/cm^2]')
+        ax.grid(visible=True)
+        fig.savefig(path_prefix + f'I_components.pdf')
+        plt.close(fig)
+
 
     # Per voltage plots
 
     voltages = next(iter(qs.values())).grid['voltage']
+    used_energy_indices = list(range(
+        0, 
+        qs['boundary0'].grid.dim_size['DeltaE'], 
+        params.plot_each_energy,
+    ))
+    energies_index_dict = {'DeltaE': used_energy_indices}
     for voltage_index, voltage in enumerate(voltages):
-        if voltage_index % params.plot_each_voltage != 0:
+        if params.plot_each_voltage <= 0 or voltage_index % params.plot_each_voltage != 0:
             continue
 
         voltage_path_prefix = f'{path_prefix}{voltage:.2f}V/'
@@ -75,20 +139,26 @@ def visualize(device):
             q_in = qs[contact.grid_name]
             in_boundary_grid = Subgrid(q_in.grid, voltage_index_dict, copy_all=False)
             q_in = restrict_quantities(q_in, in_boundary_grid)
+            in_boundary_grid_reduced = Subgrid(in_boundary_grid, energies_index_dict, copy_all=False)
+            q_in_reduced = restrict_quantities(q_in, in_boundary_grid_reduced)
             q_out = qs[contact.out_boundary_name]
             out_boundary_grid = Subgrid(q_out.grid, voltage_index_dict, copy_all=False)
             q_out = restrict_quantities(q_out, out_boundary_grid)
+            out_boundary_grid_reduced = Subgrid(out_boundary_grid, energies_index_dict, copy_all=False)
+            q_out_reduced = restrict_quantities(q_out, out_boundary_grid_reduced)
 
             # Layers
             for i in range(1,N+1):
                 q = qs[f'bulk{i}']
                 bulk_grid = Subgrid(q.grid, voltage_index_dict, copy_all=False)
                 q = restrict_quantities(q, bulk_grid)
+                bulk_grid_reduced = Subgrid(bulk_grid, energies_index_dict, copy_all=False)
+                q_reduced = restrict_quantities(q, bulk_grid_reduced)
 
                 ## Wave function
                 save_lineplot(
-                    complex_abs2(q[f'phi{i}_{contact}']),
-                    q.grid,
+                    complex_abs2(q_reduced[f'phi{i}_{contact}']),
+                    q_reduced.grid,
                     f'|phi{i}_{contact}|^2',
                     'x',
                     'DeltaE',
@@ -102,8 +172,8 @@ def visualize(device):
                 fig, ax = plt.subplots()
                 add_lineplot(
                     ax,
-                    torch.real(q[f'a_output{i}_{contact}']),
-                    q.grid,
+                    torch.real(q_reduced[f'a_output{i}_{contact}']),
+                    q_reduced.grid,
                     f'Re(a_output{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -112,8 +182,8 @@ def visualize(device):
                 )
                 add_lineplot(
                     ax,
-                    torch.imag(q[f'a_output{i}_{contact}']),
-                    q.grid,
+                    torch.imag(q_reduced[f'a_output{i}_{contact}']),
+                    q_reduced.grid,
                     f'Im(a_output{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -123,8 +193,8 @@ def visualize(device):
                 )
                 add_lineplot(
                     ax,
-                    torch.real(q[f'b_output{i}_{contact}']),
-                    q.grid,
+                    torch.real(q_reduced[f'b_output{i}_{contact}']),
+                    q_reduced.grid,
                     f'Re(b_output{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -133,8 +203,8 @@ def visualize(device):
                 )
                 add_lineplot(
                     ax,
-                    torch.imag(q[f'b_output{i}_{contact}']),
-                    q.grid,
+                    torch.imag(q_reduced[f'b_output{i}_{contact}']),
+                    q_reduced.grid,
                     f'Im(b_output{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -148,8 +218,8 @@ def visualize(device):
                 plt.close(fig)
 
                 save_lineplot(
-                    q[f'j{i}_{contact}'],
-                    q.grid,
+                    q_reduced[f'j{i}_{contact}'],
+                    q_reduced.grid,
                     f'prob_current{i}_{contact}',
                     'x',
                     'DeltaE',
@@ -179,8 +249,8 @@ def visualize(device):
                     continue
 
                 save_lineplot(
-                    torch.real(q[f'phi{i}_{contact}']),
-                    q.grid,
+                    torch.real(q_reduced[f'phi{i}_{contact}']),
+                    q_reduced.grid,
                     f'Re(phi{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -191,8 +261,8 @@ def visualize(device):
                     path_prefix = voltage_path_prefix,
                 )
                 save_lineplot(
-                    torch.imag(q[f'phi{i}_{contact}']),
-                    q.grid,
+                    torch.imag(q_reduced[f'phi{i}_{contact}']),
+                    q_reduced.grid,
                     f'Im(phi{i}_{contact})',
                     'x',
                     'DeltaE',
@@ -203,8 +273,8 @@ def visualize(device):
                     path_prefix = voltage_path_prefix,
                 )
                 visualization.save_complex_polar_plot(
-                    q[f'phi{i}_{contact}'],
-                    q.grid,
+                    q_reduced[f'phi{i}_{contact}'],
+                    q_reduced.grid,
                     f'phi{i}_{contact}',
                     'x',
                     'DeltaE',
@@ -216,8 +286,8 @@ def visualize(device):
 
                 ## a, b
                 visualization.save_complex_polar_plot(
-                    q[f'a{i}_{contact}'],
-                    q.grid,
+                    q_reduced[f'a{i}_{contact}'],
+                    q_reduced.grid,
                     f'a{i}_{contact}',
                     'x',
                     'DeltaE',
@@ -226,8 +296,8 @@ def visualize(device):
                     path_prefix = voltage_path_prefix,
                 )
                 visualization.save_complex_polar_plot(
-                    q[f'b{i}_{contact}'],
-                    q.grid,
+                    q_reduced[f'b{i}_{contact}'],
+                    q_reduced.grid,
                     f'b{i}_{contact}',
                     'x',
                     'DeltaE',
@@ -238,8 +308,8 @@ def visualize(device):
 
                 ## Losses
                 save_lineplot(
-                    q[f'SE_loss{i}_{contact}'],
-                    q.grid,
+                    q_reduced[f'SE_loss{i}_{contact}'],
+                    q_reduced.grid,
                     f'SE_loss{i}_{contact}',
                     'x',
                     'DeltaE',
@@ -262,6 +332,7 @@ def visualize(device):
                 q_in.grid,
                 'Reflection probability',
                 'DeltaE',
+                x_quantity = q_in[f'E_{contact}'],
                 x_unit=physics.EV,
                 marker = 'x',
                 linewidth = 0,
@@ -273,6 +344,7 @@ def visualize(device):
                 q_in.grid,
                 'Transmission probability',
                 'DeltaE',
+                x_quantity = q_in[f'E_{contact}'],
                 x_unit=physics.EV,
                 marker = 'x',
                 linewidth = 0,
@@ -309,6 +381,19 @@ def visualize(device):
             ax.set_xlabel('E [eV]')
             ax.set_ylim(bottom=-0.1, top=1.1)
             ax.grid(visible=True)
-            #ax.legend()
             fig.savefig(voltage_path_prefix + f'coefficients_vs_E_{contact}.pdf')
             plt.close(fig)
+
+
+            save_lineplot(
+                q_in[f'I_spectrum_{contact}'],
+                q_in.grid,
+                f'Spectral current {contact}',
+                'DeltaE',
+                x_quantity = q_in[f'E_{contact}'],
+                x_label = 'E',
+                quantity_unit = 1 / physics.CM**2 / physics.EV,
+                quantity_unit_name = 'A/cm^2/eV',
+                x_unit = physics.EV, x_unit_name = 'eV',
+                path_prefix = voltage_path_prefix,
+            )